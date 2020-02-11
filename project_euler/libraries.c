e8126aad3d867b82eee5d232d083fa9cf93a07b2
Henrik Jordheim <henrjor@stud.ntnu.no>
2020-02-11 16:38:35 +0100

    Commits

---
+++ project_euler/libraries.c
@@ -0,0 +1,11 @@
+#include <assert.h>     //Program assertions functions
+#include <ctype.h>      //Character type functions
+#include <locale.h>     //Localization functions
+#include <math.h>       //Mathematics functions
+#include <setjmp.h>     //Jump functions
+#include <signal.h>     //Signal handling functions
+#include <stdarg.h>     //Variable arguments handling functions
+#include <stdio.h>      //Stabdard Input/Output
+#include <stdlib.h>     //Standard Utility functions
+#include <string.h>     //String handling functions
+#include <time.h>       //Date time functions
---
+++ project_euler/problem_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct node{
+    int value;
+    struct node * next;
+} node_t;
+
+node_t * head = NULL;
+head = malloc(sizeof(node_t));
+if(head == NULL)[
+
+]
+
+
+
+
+
+int main(){
+    printf("Hello World!\n");
+    int array[] = {1,2,3,4,5};
+    print(array);
+
+    return 0;
+}
